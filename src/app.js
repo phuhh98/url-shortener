e5c9ec938f386133907e2075ed54116dc9755d29
Phu Huynh <phu_huynh@epam.com>
2022-07-13 11:01:40 +0700

    reformat

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-//require("dotenv").config(); // load env var from .env for dev test
-require("./db/mongoose.js"); //establish connect with mongodb
-const express = require("express");
-const shortURL = require("./routers/shortURL.js");
-const path = require("path");
+require('dotenv').config(); // load env var from .env for dev test
+require('./db/mongoose.js'); //establish connect with mongodb
+const express = require('express');
+const shortURL = require('./routers/shortURL.js');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const viewsPath = path.join(__dirname, "../templates/views");
-const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, '../templates/views');
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.set("view engine", "pug");
-app.set("views", viewsPath);
+app.set('view engine', 'pug');
+app.set('views', viewsPath);
 //Set up static files directory to serve
 app.use(express.static(publicDirectoryPath));
 app.use(express.json());
@@ -18,12 +18,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(shortURL); // /url/ router
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   //render index view with a form
-  res.render("index");
+  res.render('index');
 });
 
-app.get("/:shortId", (req, res) => {
+app.get('/:shortId', (req, res) => {
   //console.log(req.params.shortId);
   if (req.params.shortId) {
     res.redirect(`/url/${req.params.shortId}`);
